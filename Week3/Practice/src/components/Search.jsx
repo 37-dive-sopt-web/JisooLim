a2e173e5d185d311fa00915a653df1b773e2679d
임지수 <dlawltn0708@naver.com>
2025-11-01 17:23:54 +0900

    feat: 커스텀훅 실습

--- Week3/Practice/src/components/Search.jsx
+++ Week3/Practice/src/components/Search.jsx
@@ -1,10 +1,15 @@
-const Search = ({search, handleSearchChange}) => {
-    return (
+const Search = ({ search, handleSearchChange, handleSearch }) => {
+  return (
     <div>
-      <input type="text" placeholder="검색어를 입력하세요" value={search} onChange={handleSearchChange}/>
-      <button>검색</button>
+      <input
+        type='text'
+        value={search}
+        onChange={handleSearchChange}
+        placeholder='검색어를 입력하세요'
+      />
+      <button onClick={handleSearch}>검색</button>
     </div>
   );
-}
+};
 
 export default Search;
