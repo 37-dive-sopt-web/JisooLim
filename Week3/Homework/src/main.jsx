69cd599b727b196fbd38c85ec5ed293e969fda5c
임지수 <dlawltn0708@naver.com>
2025-11-04 11:20:31 +0900

    chore: tailwindcss 세팅

--- Week3/Homework/src/main.jsx
+++ Week3/Homework/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import 'modern-css-reset/dist/reset.min.css';
+import 'modern-css-reset';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
