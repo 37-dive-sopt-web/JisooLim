cc32a21aff013c4b11f666431dbe573b9d0762c3
임지수 <dlawltn0708@naver.com>
2025-11-10 22:10:11 +0900

    refactor: 회원가입 상수 분리

--- Week4/Homework/src/pages/signup/SignupPage.tsx
+++ Week4/Homework/src/pages/signup/SignupPage.tsx
@@ -1,76 +1,9 @@
 import { useState } from "react";
 import { Link } from "react-router";
 import Button from "@/shared/components/button/Button";
-import Input, { type InputProps } from "@/shared/components/input/Input";
+import Input from "@/shared/components/input/Input";
+import { STEP_FLOW } from "./flow";
 import * as s from "./SignupPage.css";
-
-type StepId = "account" | "password" | "profile";
-
-interface StepDefinition {
-  id: StepId;
-  fields: InputProps[];
-}
-
-const STEP_FLOW: StepDefinition[] = [
-  {
-    id: "account",
-    fields: [
-      {
-        id: "signup-id",
-        name: "id",
-        label: "아이디",
-        placeholder: "아이디를 입력하세요",
-      },
-    ],
-  },
-  {
-    id: "password",
-    fields: [
-      {
-        id: "signup-password",
-        name: "password",
-        type: "password",
-        label: "비밀번호",
-        placeholder: "비밀번호를 입력하세요",
-        toggleVisibility: true,
-      },
-      {
-        id: "signup-password-confirm",
-        name: "passwordConfirm",
-        type: "password",
-        label: "비밀번호 확인",
-        placeholder: "다시 한 번 입력하세요",
-        toggleVisibility: true,
-      },
-    ],
-  },
-  {
-    id: "profile",
-    fields: [
-      {
-        id: "signup-name",
-        name: "name",
-        label: "이름",
-        placeholder: "이름을 입력하세요",
-      },
-      {
-        id: "signup-email",
-        name: "email",
-        type: "email",
-        label: "이메일",
-        placeholder: "example@email.com",
-      },
-      {
-        id: "signup-age",
-        name: "age",
-        type: "number",
-        min: 0,
-        label: "나이",
-        placeholder: "나이를 입력하세요",
-      },
-    ],
-  },
-];
 
 const SignupPage = () => {
   const [stepIndex, setStepIndex] = useState(0);
@@ -119,7 +52,7 @@ const SignupPage = () => {
           이미 계정이 있나요?
           <Link className={s.backLink} to="/">
             {" "}
-            로그인으로 돌아가기
+            로그인하러 가기
           </Link>
         </p>
       </section>
